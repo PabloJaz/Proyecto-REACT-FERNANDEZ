37a8c0b5915bea3f2b0ef8e74e838b9bf49c6117
Pablo <123111904+PabloJaz@users.noreply.github.com>
2023-10-06 14:18:48 -0300

    first commit
    first commit

---
+++ src/services/FireStore.js
@@ -0,0 +1,82 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  addDoc,
+  query,
+  where,
+  collection,
+  getDocs,
+  Timestamp,
+} from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_pf_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const fireStoreDb = getFirestore(app);
+
+export default fireStoreDb;
+
+// Obtengo todos los items de la DB
+
+export async function getAllItems() {
+  const miColec = collection(fireStoreDb, "item");
+  const itemsSnapshot = await getDocs(miColec);
+
+  return itemsSnapshot.docs.map((doc) => {
+    return {
+      ...doc.data(),
+      id: doc.id,
+    };
+  });
+}
+
+export async function getItemsByCategory(category) {
+  const miColec = collection(fireStoreDb, "item");
+  const queryItem = query(miColec, where("category", "==", category));
+  const itemSnapshot = await getDocs(queryItem);
+
+  return itemSnapshot.docs.map((doc) => {
+    return {
+      ...doc.data(),
+      id: doc.id,
+    };
+  });
+}
+
+export async function getItem(id) {
+  const miColec = collection(fireStoreDb, "item");
+  const itemRef = doc(miColec, id);
+  const itemSnapshot = await getDoc(itemRef);
+
+  return {
+    ...itemSnapshot.data(),
+    id: itemSnapshot.id,
+  };
+}
+
+export async function createBuyOrder(orderData) {
+  const buyTimeStamp = Timestamp.now();
+  const orderWithDate = {
+    ...orderData,
+    date: buyTimeStamp,
+  };
+  const miColec = collection(fireStoreDb, "buyOrders");
+  const orderDoc = await addDoc(miColec, orderWithDate);
+  console.log("Orden lista con el id ", orderDoc.id);
+  return orderDoc.id;
+}
